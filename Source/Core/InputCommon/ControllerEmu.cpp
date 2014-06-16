bcfaba16fee68185bdc145ac72b8665df5a34fd9
Jasper St. Pierre <jstpierre@mecheye.net>
2014-06-16 10:13:24 -0400

    ControllerEmu: Move the implementation of GetState into the .cpp file
    This is where it belongs and makes the code more maintainable and faster
    to compile and yadda yadda yadda why was this ever in the .h file???
    What?
    
    There should not be any code changes, just a code move.

--- Source/Core/InputCommon/ControllerEmu.cpp
+++ Source/Core/InputCommon/ControllerEmu.cpp
@@ -148,9 +148,78 @@ ControllerEmu::AnalogStick::AnalogStick(const char* const _name) : ControlGroup(
 
 }
 
+template <typename C>
+void ControllerEmu::AnalogStick::GetState(C* const x, C* const y, const unsigned int base, const unsigned int range)
+{
+	// this is all a mess
+
+	ControlState yy = controls[0]->control_ref->State() - controls[1]->control_ref->State();
+	ControlState xx = controls[3]->control_ref->State() - controls[2]->control_ref->State();
+
+	ControlState radius = settings[SETTING_RADIUS]->value;
+	ControlState deadzone = settings[SETTING_DEADZONE]->value;
+	ControlState square = settings[SETTING_SQUARE]->value;
+	ControlState m = controls[4]->control_ref->State();
+
+	// modifier code
+	if (m)
+	{
+		yy = (fabsf(yy)>deadzone) * sign(yy) * (m + deadzone/2);
+		xx = (fabsf(xx)>deadzone) * sign(xx) * (m + deadzone/2);
+	}
+
+	// deadzone / square stick code
+	if (radius != 1 || deadzone || square)
+	{
+		// this section might be all wrong, but its working good enough, I think
+
+		ControlState ang = atan2(yy, xx);
+		ControlState ang_sin = sin(ang);
+		ControlState ang_cos = cos(ang);
+
+		// the amt a full square stick would have at current angle
+		ControlState square_full = std::min(ang_sin ? 1/fabsf(ang_sin) : 2, ang_cos ? 1/fabsf(ang_cos) : 2);
+
+		// the amt a full stick would have that was ( user setting squareness) at current angle
+		// I think this is more like a pointed circle rather than a rounded square like it should be
+		ControlState stick_full = (1 + (square_full - 1) * square);
+
+		ControlState dist = sqrt(xx*xx + yy*yy);
+
+		// dead zone code
+		dist = std::max(0.0f, dist - deadzone * stick_full);
+		dist /= (1 - deadzone);
+
+		// square stick code
+		ControlState amt = dist / stick_full;
+		dist -= ((square_full - 1) * amt * square);
+
+		// radius
+		dist *= radius;
+
+		yy = std::max(-1.0f, std::min(1.0f, ang_sin * dist));
+		xx = std::max(-1.0f, std::min(1.0f, ang_cos * dist));
+	}
+
+	*y = C(yy * range + base);
+	*x = C(xx * range + base);
+}
+
 ControllerEmu::Buttons::Buttons(const std::string& _name) : ControlGroup(_name, GROUP_TYPE_BUTTONS)
 {
 	settings.emplace_back(new Setting(_trans("Threshold"), 0.5f));
+}
+
+template <typename C>
+void ControllerEmu::Buttons::GetState(C* const buttons, const C* bitmasks)
+{
+	for (auto& control : controls)
+	{
+		if (control->control_ref->State() > settings[0]->value) // threshold
+			*buttons |= *bitmasks;
+
+		bitmasks++;
+	}
 }
 
 ControllerEmu::MixedTriggers::MixedTriggers(const std::string& _name) : ControlGroup(_name, GROUP_TYPE_MIXED_TRIGGERS)
@@ -158,9 +227,36 @@ ControllerEmu::MixedTriggers::MixedTriggers(const std::string& _name) : ControlG
 	settings.emplace_back(new Setting(_trans("Threshold"), 0.9f));
 }
 
+template <typename C, typename S>
+void ControllerEmu::MixedTriggers::GetState(C* const digital, const C* bitmasks, S* analog, const unsigned int range)
+{
+	const unsigned int trig_count = ((unsigned int) (controls.size() / 2));
+	for (unsigned int i=0; i<trig_count; ++i,++bitmasks,++analog)
+	{
+		if (controls[i]->control_ref->State() > settings[0]->value) //threshold
+		{
+			*analog = range;
+			*digital |= *bitmasks;
+		}
+		else
+		{
+			*analog = S(controls[i+trig_count]->control_ref->State() * range);
+		}
+	}
+}
+
 ControllerEmu::Triggers::Triggers(const std::string& _name) : ControlGroup(_name, GROUP_TYPE_TRIGGERS)
 {
 	settings.emplace_back(new Setting(_trans("Dead Zone"), 0, 0, 50));
+}
+
+template <typename S>
+void ControllerEmu::Triggers::GetState(S* analog, const unsigned int range)
+{
+	const unsigned int trig_count = ((unsigned int) (controls.size()));
+	const ControlState deadzone = settings[0]->value;
+	for (unsigned int i=0; i<trig_count; ++i,++analog)
+		*analog = S(std::max(controls[i]->control_ref->State() - deadzone, 0.0f) / (1 - deadzone) * range);
 }
 
 ControllerEmu::Slider::Slider(const std::string& _name) : ControlGroup(_name, GROUP_TYPE_SLIDER)
@@ -169,6 +265,18 @@ ControllerEmu::Slider::Slider(const std::string& _name) : ControlGroup(_name, GR
 	controls.emplace_back(new Input("Right"));
 
 	settings.emplace_back(new Setting(_trans("Dead Zone"), 0, 0, 50));
+}
+
+template <typename S>
+void ControllerEmu::Slider::GetState(S* const slider, const unsigned int range, const unsigned int base /* = 0 */)
+{
+	const float deadzone = settings[0]->value;
+	const float state = controls[1]->control_ref->State() - controls[0]->control_ref->State();
+
+	if (fabsf(state) > deadzone)
+		*slider = (S)((state - (deadzone * sign(state))) / (1 - deadzone) * range + base);
+	else
+		*slider = 0;
 }
 
 ControllerEmu::Force::Force(const std::string& _name) : ControlGroup(_name, GROUP_TYPE_FORCE)
@@ -183,6 +291,23 @@ ControllerEmu::Force::Force(const std::string& _name) : ControlGroup(_name, GROU
 	controls.emplace_back(new Input(_trans("Backward")));
 
 	settings.emplace_back(new Setting(_trans("Dead Zone"), 0, 0, 50));
+}
+
+template <typename C, typename R>
+void ControllerEmu::Force::GetState(C* axis, const u8 base, const R range)
+{
+	const float deadzone = settings[0]->value;
+	for (unsigned int i=0; i<6; i+=2)
+	{
+		float tmpf = 0;
+		const float state = controls[i+1]->control_ref->State() - controls[i]->control_ref->State();
+		if (fabsf(state) > deadzone)
+			tmpf = ((state - (deadzone * sign(state))) / (1 - deadzone));
+
+		float &ax = m_swing[i >> 1];
+		*axis++ = (C)((tmpf - ax) * range + base);
+		ax = tmpf;
+	}
 }
 
 ControllerEmu::Tilt::Tilt(const std::string& _name)
@@ -202,6 +327,77 @@ ControllerEmu::Tilt::Tilt(const std::string& _name)
 	settings.emplace_back(new Setting(_trans("Angle"), 0.9f, 0, 180));
 }
 
+template <typename C, typename R>
+void ControllerEmu::Tilt::GetState(C* const x, C* const y, const unsigned int base, const R range, const bool step /* = true */)
+{
+	// this is all a mess
+
+	ControlState yy = controls[0]->control_ref->State() - controls[1]->control_ref->State();
+	ControlState xx = controls[3]->control_ref->State() - controls[2]->control_ref->State();
+
+	ControlState deadzone = settings[0]->value;
+	ControlState circle = settings[1]->value;
+	auto const angle = settings[2]->value / 1.8f;
+	ControlState m = controls[4]->control_ref->State();
+
+	// modifier code
+	if (m)
+	{
+		yy = (fabsf(yy)>deadzone) * sign(yy) * (m + deadzone/2);
+		xx = (fabsf(xx)>deadzone) * sign(xx) * (m + deadzone/2);
+	}
+
+	// deadzone / circle stick code
+	if (deadzone || circle)
+	{
+		// this section might be all wrong, but its working good enough, I think
+
+		ControlState ang = atan2(yy, xx);
+		ControlState ang_sin = sin(ang);
+		ControlState ang_cos = cos(ang);
+
+		// the amt a full square stick would have at current angle
+		ControlState square_full = std::min(ang_sin ? 1/fabsf(ang_sin) : 2, ang_cos ? 1/fabsf(ang_cos) : 2);
+
+		// the amt a full stick would have that was (user setting circular) at current angle
+		// I think this is more like a pointed circle rather than a rounded square like it should be
+		ControlState stick_full = (square_full * (1 - circle)) + (circle);
+
+		ControlState dist = sqrt(xx*xx + yy*yy);
+
+		// dead zone code
+		dist = std::max(0.0f, dist - deadzone * stick_full);
+		dist /= (1 - deadzone);
+
+		// circle stick code
+		ControlState amt = dist / stick_full;
+		dist += (square_full - 1) * amt * circle;
+
+		yy = std::max(-1.0f, std::min(1.0f, ang_sin * dist));
+		xx = std::max(-1.0f, std::min(1.0f, ang_cos * dist));
+	}
+
+	// this is kinda silly here
+	// gui being open will make this happen 2x as fast, o well
+
+	// silly
+	if (step)
+	{
+		if (xx > m_tilt[0])
+			m_tilt[0] = std::min(m_tilt[0] + 0.1f, xx);
+		else if (xx < m_tilt[0])
+			m_tilt[0] = std::max(m_tilt[0] - 0.1f, xx);
+
+		if (yy > m_tilt[1])
+			m_tilt[1] = std::min(m_tilt[1] + 0.1f, yy);
+		else if (yy < m_tilt[1])
+			m_tilt[1] = std::max(m_tilt[1] - 0.1f, yy);
+	}
+
+	*y = C(m_tilt[1] * angle * range + base);
+	*x = C(m_tilt[0] * angle * range + base);
+}
+
 ControllerEmu::Cursor::Cursor(const std::string& _name)
 	: ControlGroup(_name, GROUP_TYPE_CURSOR)
 	, m_z(0)
@@ -216,6 +412,42 @@ ControllerEmu::Cursor::Cursor(const std::string& _name)
 	settings.emplace_back(new Setting(_trans("Width"), 0.5f));
 	settings.emplace_back(new Setting(_trans("Height"), 0.5f));
 
+}
+
+template <typename C>
+void ControllerEmu::Cursor::GetState(C* const x, C* const y, C* const z, const bool adjusted /* = false */)
+{
+	const float zz = controls[4]->control_ref->State() - controls[5]->control_ref->State();
+
+	// silly being here
+	if (zz > m_z)
+		m_z = std::min(m_z + 0.1f, zz);
+	else if (zz < m_z)
+		m_z = std::max(m_z - 0.1f, zz);
+
+	*z = m_z;
+
+	// hide
+	if (controls[6]->control_ref->State() > 0.5f)
+	{
+		*x = 10000; *y = 0;
+	}
+	else
+	{
+		float yy = controls[0]->control_ref->State() - controls[1]->control_ref->State();
+		float xx = controls[3]->control_ref->State() - controls[2]->control_ref->State();
+
+		// adjust cursor according to settings
+		if (adjusted)
+		{
+			xx *= (settings[1]->value * 2);
+			yy *= (settings[2]->value * 2);
+			yy += (settings[0]->value - 0.5f);
+		}
+
+		*x = xx;
+		*y = yy;
+	}
 }
 
 void ControllerEmu::LoadDefaults(const ControllerInterface &ciface)
